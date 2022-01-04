ac1ae09a00da5fa55d4e1002685cbbc3270f4377
spencer <spencerhhubert@gmail.com>
2022-01-03 23:46:20 -0800

    import pics to js function complete

--- file_list.py
+++ file_list.py
@@ -1,8 +1,12 @@
 import os
 
+img_dir = "data/images/"
+label_dir = "data/labels/"
 js_line = "";
-for img in os.listdir("data/images/"):
-	js_line += f"{img}, ";
+for part in sorted(os.listdir(img_dir)):
+	part_dir = img_dir+part+'/';
+	for img in sorted(os.listdir(part_dir)):
+		js_line += f"{part_dir+img}, ";
 js_line = f'let list = "{js_line}";\n';
 
 with open("crayon.js") as f:
